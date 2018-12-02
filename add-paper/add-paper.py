cb0413c943ce4509c6817be97eff249c611e75d3
Goderich <yuhsien77@gmail.com>
2018-12-02 23:10:41 +0800

    Add types to add-paper.py

--- add-paper/add-paper.py
+++ add-paper/add-paper.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import subprocess
+import yaml
 
 PAPIS_DIR = '/home/iwaka/Documents/papers/'
 
@@ -25,13 +26,24 @@ def main(filename):
 
     papis_add(new_file_name, name_year_title)
 
-    add_year_to_yaml(new_dir_name, year)
+    info_yaml = open(PAPIS_DIR + new_dir_name + '/info.yaml', 'r')
+    yaml_data = yaml.safe_load(info_yaml)
+    info_yaml.close()
+
+    # do stuff with YAML file
+    yaml_data['year'] = year
+    yaml_data['type'] = bibtex_type
+
+    info_yaml_write = open(PAPIS_DIR + new_dir_name + '/info.yaml', 'w')
+    yaml.dump(yaml_data, info_yaml_write)
+    info_yaml_write.close()
+
     print('Added ' + new_file_name + ' to papis library.')
 
     remove_original_file(new_file_name)
 
 
-def create_dir_name(name_year_title):
+def create_dir_name(name_year_title: (str, str, str)) -> str:
     '''
     last_name and formatted_year are either empty strings,
     or end with an underscore for name formatting
@@ -45,7 +57,8 @@ def create_dir_name(name_year_title):
     return last_name + formatted_year + formatted_title
 
 
-def get_author_last_name(author_name):
+def get_author_last_name(author_name: str) -> str:
+    '''Any spaces are replaced with _ for directory naming'''
     if author_name != '':
         last_name = author_name.split(',')[0]
         last_name = last_name.replace(' ', '_')
@@ -54,14 +67,14 @@ def get_author_last_name(author_name):
         return ''
 
 
-def get_formatted_year(year):
+def get_formatted_year(year: str) -> str:
     if year != '':
         return year + '_'
     else:
         return ''
 
 
-def get_formatted_title(title):
+def get_formatted_title(title: str) -> str:
     if len(title) > 50:
         primary_title = title.split(':')[0]
         return strip_punctuation_and_spaces(primary_title)
@@ -69,38 +82,37 @@ def get_formatted_title(title):
         return strip_punctuation_and_spaces(title)
 
 
-def get_bibtex_type(letter):
-    if letter.lower() == 'a' or letter == '':
-        return 'article'
-    elif letter.lower() == 'b':
-        return 'book'
-    elif letter.lower() == 'c':
-        return 'incollection'
-    elif letter.lower() == 'p':
-        return 'inproceedings'
-    elif letter.lower() == 's':
-        return 'abstract'
-    elif letter.lower() == 'r':
-        return 'presentation'
-    elif letter.lower() == 'm':
-        return 'mastersthesis'
-    elif letter.lower() == 'd':
-        return 'phdthesis'
-    else:
+def get_bibtex_type(letter: str) -> str:
+    lowercase_letter = letter.lower()
+    bibtex_types = {
+        'a': 'article',
+        '':  'article',
+        'b': 'book',
+        'c': 'incollection',
+        'p': 'inproceedings',
+        's': 'abstract',
+        'r': 'presentation',
+        'm': 'mastersthesis',
+        'd': 'phdthesis',
+        }
+
+    try:
+        return bibtex_types[lowercase_letter]
+    except KeyError:
         return ''
 
 
-def strip_punctuation_and_spaces(string):
+def strip_punctuation_and_spaces(string: str) -> str:
     return ''.join(x for x in string
                    if x.isalnum() or x.isspace
                    ).replace(' ', '_')
 
 
-def rename_pdf(filename, new_name):
+def rename_pdf(filename, new_name) -> None:
     os.rename(filename, new_name)
 
 
-def papis_add(filename, name_year_title):
+def papis_add(filename, name_year_title) -> None:
     author_name, year, title = name_year_title
     dir_name = filename[:-4]
     subprocess.run(['papis', 'add', filename,
@@ -109,13 +121,7 @@ def papis_add(filename, name_year_title):
                     '--name', dir_name])
 
 
-def add_year_to_yaml(dir_name, year):
-    info_file = open(PAPIS_DIR + dir_name + '/info.yaml', 'a')
-    info_file.write("year: '" + year + "'")
-    info_file.close()
-
-
-def remove_original_file(filename):
+def remove_original_file(filename) -> None:
     os.remove(filename)
 
 
