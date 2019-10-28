503e480c34200b228902ba2998f320ca6cf8777a
Goderich <yuhsien77@gmail.com>
2019-10-28 11:59:02 +0800

    Add CSV diffing function
    I start with a hard-coded 'original' file, and diff all args with it.
    The merging algorithm adds the args to the 'original' file one by one.
    The keys that exist in the original but not in the appended file are
    sent to the diff files. The keys that don't exist in the original
    are dropped.

--- csv-combinator/app/Main.hs
+++ csv-combinator/app/Main.hs
@@ -6,7 +6,9 @@ import qualified Data.ByteString.Lazy as BL
 import Data.Csv
 import qualified Data.Vector as V
 import qualified Data.Map.Strict as M
+import qualified Data.Map.Merge.Strict as M
 import System.Environment (getArgs)
+import Control.Monad (zipWithM_)
 
 type Key = (String, String)
 type Glosses = [String]
@@ -31,7 +33,7 @@ instance ToRecord Lemma where
         record (toField d : toField m : map toField gs)
 
 fromLemmata :: V.Vector Lemma -> M.Map Key [Glosses]
-fromLemmata = M.fromListWith (flip (++)) . V.toList . V.map toKey 
+fromLemmata = M.fromListWith (flip (++)) . V.toList . fmap toKey 
     where toKey (Lemma d m r) = ((d,m), [r])
 
 toLemmata :: M.Map Key [Glosses] -> V.Vector Lemma
@@ -66,18 +68,48 @@ transformCSV :: Either String (V.Vector Lemma)
 transformCSV xs ys = do
     v1 <- xs
     v2 <- ys
+    -- l = total amount of gloss columns in two tables
+    let l = sum . map (length . rest . V.head) $ [v1, v2]
     let m1 = fromLemmata v1
     let m2 = fromLemmata v2
-    let m3 = M.unionWith combineLs m1 m2
-    pure $ toLemmata m3
+    let m3 = M.merge
+               -- Preserve keys only found in m1
+               M.preserveMissing
+               -- Drop keys only found in m2
+               M.dropMissing
+               (M.zipWithMatched (\_ x y -> combineLs x y))
+               m1 m2
+    pure . fmap (padLemma l) . toLemmata $ m3
+
+-- Makes sure every row has the same number of columns.
+-- Only need to pad right because left is padded in combineLs
+padLemma :: Int -> Lemma -> Lemma
+padLemma l (Lemma d m gs) = Lemma d m (gs ++ pad gs l)
+
+diffCSVs :: Either String (V.Vector Lemma)
+         -> Either String (V.Vector Lemma)
+         -> Either String (V.Vector Lemma)
+diffCSVs xs ys = do
+    v1 <- xs
+    v2 <- ys
+    let m1 = fromLemmata v1
+    let m2 = fromLemmata v2
+    let diff = M.difference m1 m2
+    pure . toLemmata $ diff
+
+printCSV :: Either String (V.Vector Lemma) -> String -> IO ()
+printCSV x name = case x of
+    Left err -> putStrLn err
+    Right r  -> BL.writeFile name . encode . V.toList $ r
 
 main :: IO ()
 main = do
     args <- getArgs
     files <- mapM BL.readFile args
+    origF <- BL.readFile "test/original.csv"
+    let original = decode HasHeader origF
     let csvs = map (decode HasHeader) files
-    let result = foldl1 transformCSV csvs
-    case result of
-        Left err -> putStrLn err
-        Right r  -> BL.writeFile "test/output.csv"
-                  . encode . V.toList $ r
+    let diffs = map (diffCSVs original) csvs
+    zipWithM_ printCSV diffs (map (++ "-diff") args)
+    let merged = foldl transformCSV original csvs
+    printCSV merged "test/output.csv"
